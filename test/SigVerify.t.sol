86e1accfab48738f9c1d9f166f5ce7d4cf41b76f
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-10-18 20:02:27 +0100

    imported contracts

--- test/SigVerify.t.sol
+++ test/SigVerify.t.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import "../src/SigVerify.sol";
+import "../src/Token.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
 
 contract SignatureVerificationTest is Test {
     SigVerify public verifier;
